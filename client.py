2c3568c80daea7b8d7a88707d0f3e78321a91faf
albioninnovate <ward@wardhills.net>
2020-12-08 23:54:54 +0000

    full data path established, not alway returing Euler angels.

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import configparser
 import ast
-import requests
+
 
 
 logging.basicConfig(format='%(asctime)s %(message)s',
@@ -14,40 +14,23 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 
-
 async def tcp_echo_client(message):
 #    reader, writer = await asyncio.open_connection(
 #        '127.0.0.1', 8888)
 
-    reader, writer = await asyncio.open_connection(
-        '192.168.1.39', 8888)
+    reader, writer = await asyncio.open_connection('192.168.1.39', 8888)
 
-    print(f'Send: {message!r}')
+    #print(f'Send: {message!r}')
     writer.write(message.encode())
 
     n = int(config['DATA']['characters'])
     data = await reader.read(n)
-    print(f'Received: {data.decode()!r}')
+    #print(f'Received: {data.decode()!r}')
 
     try:
         b_dict = ast.literal_eval(data.decode())  # extract the dictionary from the string received
         #print(b_dict)
         print('Euler angles : ', b_dict['Euler angle'])
-
-        _az = b_dict['Euler angle'][0]
-        _alt= b_dict['Euler angle'][1]
-        
-        print('Az =', _az)
-        print('alt =', _alt)
-
-        # api-endpoint 
-        URL = "http://localhost:8090/api/main/view"
-        
-        # defining a params dict for the parameters to be sent to the API
-        PARAMS = {"az":_az, "alt": _alt}
-
-        # sending get request and saving the response as response object 
-        r = requests.post(url = URL, params = PARAMS) 
 
     except:
         print('Some values returned Null, not able to extract data')
@@ -58,4 +41,11 @@ async def tcp_echo_client(message):
         print('Close the connection')
         writer.close()
 
-asyncio.run(tcp_echo_client('Hello World!'))
+    return b_dict
+
+def main():
+    received  = asyncio.run(tcp_echo_client('Hello World!'))
+    return received
+
+if __name__ == '__main__':
+    main()
