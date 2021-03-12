cd9b9cebefe480b3b4dc278a1d377decf633d014
albioninnovate <mail@wardhills.net>
2021-03-12 23:32:39 +0000

    mod ard_srv.py to PICO

--- rpi/ard_ser.py
+++ rpi/ard_ser.py
@@ -45,10 +45,13 @@ def read(output_format='dict'):
 
     try:
         ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+        #ser = serial.Serial('/dev/ttyACM1', 115200, timeout=1)
         # logging.debug('/dev/ttyACM0')
 
     except:
-        ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+        #ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+        #ser = serial.Serial('/dev/ttyACM1', 115200, timeout=1)
+        ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
         #logging.debug('/dev/ttyUSB0')
 
     ser.flush()
@@ -61,35 +64,57 @@ def read(output_format='dict'):
                 #print(line)
 
                 s = line
+                
+                #print(s)
 
+                # readings = {
+                         # 'X'       : s.split('X:',-1)[1].split()[0],
+                         # 'Y'       : s.split('Y:',-1)[1].split()[0],
+                         # 'Z'       : s.split('Z:',-1)[1].split()[0],
+                        # 'qW'      : s.split('qW:',-1)[1].split()[0],
+                        # 'qX'      : s.split('qX:',-1)[1].split()[0],
+                        # 'qY'      : s.split('qY:',-1)[1].split()[0],
+                        # 'qZ'      : s.split('qZ:', -1)[1].split()[0],
+                        # 'Sys_cal' : s.split('Sys=',-1)[1].split()[0],
+                        # 'G_cal'   : s.split('Gyro=',-1)[1].split()[0],
+                        # 'A_cal'   : s.split('Accel=',-1)[1].split()[0],
+                        # 'M_cal'   : s.split('Mag=',-1)[1].split()[0]
+                 # }
+                 
+                 
                 readings = {
                          'X'       : s.split('X:',-1)[1].split()[0],
                          'Y'       : s.split('Y:',-1)[1].split()[0],
-                         'Z'       : s.split('Z:',-1)[1].split()[0],
-                        'qW'      : s.split('qW:',-1)[1].split()[0],
-                        'qX'      : s.split('qX:',-1)[1].split()[0],
-                        'qY'      : s.split('qY:',-1)[1].split()[0],
-                        'qZ'      : s.split('qZ:', -1)[1].split()[0],
-                        'Sys_cal' : s.split('Sys=',-1)[1].split()[0],
-                        'G_cal'   : s.split('Gyro=',-1)[1].split()[0],
-                        'A_cal'   : s.split('Accel=',-1)[1].split()[0],
-                        'M_cal'   : s.split('Mag=',-1)[1].split()[0]
+                         'Z'       : s.split('Z:',-1)[1].split()[0]
                  }
+                 
+                #print(readings)
+                #print(output_format)
+                
+                if output_format == 'json':
+                    #print('jason')
+                    readings = to_json(readings)
+            
+                return readings
 
         except Exception as e:
             logging.debug(e)
             print(e)
             pass
 
-        if 'X' in readings.keys():
-            s = float(readings['Sys_cal'])
-            if s >= 0:
-                if output_format == 'json':
-                    readings = to_json(readings)
-                return readings
+        # ~ if 'X' in readings.keys():
+            # ~ s = float(readings['Sys_cal'])
+            # ~ if s >= 0:
+                # ~ if output_format == 'json':
+                    # ~ readings = to_json(readings)
+                # ~ return readings
+    
 
 
 if __name__ == '__main__':
     while True:
         r = read()
-        print(r)
+        print('as dict : ',r)
+        
+        j = read(output_format='json')
+        print('as JSON :',j)
