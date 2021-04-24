3aa096be04e1af78e331f19bfa9329fffc1e739f
albioninnovate <mail@wardhills.net>
2021-04-24 21:05:50 +0100

    start server at boot tested

--- rpi/server.py
+++ rpi/server.py
@@ -6,17 +6,12 @@ import logging
 import configparser
 #import ard_ser
 import pico_ser
+import socket
+
 
 
 # TODO set this to run when the pi boots
-# TODO combine the log files for server.py and ard_ser.py
 #
-# filename='server.log'
-# level = 'logging.DEBUG'
-#
-# logging.basicConfig(format='%(asctime)s %(lineno)d %(message)s',
-#                     filename=filename,
-#                     level=level)
 
 # Read config file
 config = configparser.ConfigParser()
@@ -35,39 +30,46 @@ async def handle_echo(reader, writer):
     #sensor = ard_ser.read(output_format='json')
     sensor = pico_ser.read(output_format='json')
 
-    # logging.info('Read sensor')
-    #print('sensor', sensor)
-
     data = sensor
 
     addr = writer.get_extra_info('peername')
-    # logging.info(addr)
 
-    print('Received', message, 'from',addr)
-    #logging.info('Received', {}, 'from', addr)
 
-    print('Send: ' , message)
+    #print('Received', message, 'from',addr)
+
+
+    #print('Send: ' , message)
     writer.write(data)
     await writer.drain()
 
-    print("Close the connection")
+    #print("Close the connection")
     writer.close()
-    # logging.info('Closed connection')
+
 
 async def main():
 #   server = await asyncio.start_server(
 #       handle_echo, '192.168.1.39', 8888)
 
-#    server = await asyncio.start_server(
-#        handle_echo, 'freeside.local', 8888)
-   server = await asyncio.start_server(
-       handle_echo, '169.254.230.229', 8888)
 
-   addr = server.sockets[0].getsockname()
-   print('Serving on ', addr)
-   # logging.info('Server started on {}'.format(addr))
+#   server = await asyncio.start_server(
+#       handle_echo, '169.254.230.229', 8888)
 
-   async with server:
+    server = await asyncio.start_server(
+       handle_echo, '169.254.162.167', 8888)
+
+#    hostname = socket.gethostname()   
+#    print(hostname)
+#    ipaddr = socket.gethostbyname(hostname+'.local')   
+    #ipaddr = socket.gethostbyname(hostname)
+#    print(ipaddr)
+
+   # server = await asyncio.start_server(
+   #    handle_echo, ipaddr, 8888)
+
+    addr = server.sockets[0].getsockname()
+    print('Serving on ', addr)
+  
+    async with server:
         await server.serve_forever()
 
 asyncio.run(main())
